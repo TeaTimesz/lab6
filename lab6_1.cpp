5a71967456cc8ba7bd635dda13dca7278b113bc5
TeaTimesz <rawiphatmak@gmail.com>
2024-11-29 16:27:08 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int temp;
+    int num[2]={};
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> temp;
+
+    while(temp != 0){
+        if(temp % 2 == 0){
+            num[0]++;
+        }else{
+            num[1]++;
+        }
+        cout << "Enter an integer: ";
+        cin >> temp;
+    }   
+
+    cout << "#Even numbers = " << num[0] << endl;
+    cout << "#Odd numbers = " << num[1] << endl;
     return 0;
 }
